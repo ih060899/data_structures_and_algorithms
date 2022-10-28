3bbc6b4ebc10bf9a05d1d3c03be1a2146b2b1ff2
imran <ih060899@gmail.com>
2022-10-29 01:11:18 +0600

    arrays implementation

---
+++ src/arrays/ArrayMain.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.removeAt(0);
+        numbers.print();
+    }
+}
